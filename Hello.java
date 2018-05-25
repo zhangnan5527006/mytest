9a0904fbd91f4643182047cf042b00939b647dfb
zhangnan <sailingunionpc@163.com>
2018-05-25 21:10:37 +0800

    the second update

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
 	
 	public static void main(String args[]){
-		system.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello Zhangnan");
 	}
 	
 }
