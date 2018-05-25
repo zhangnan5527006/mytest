7ddf124cdb6668329913798fb5613d3393ea6419
zhangnan <sailingunionpc@163.com>
2018-05-25 21:22:05 +0800

    add Demo.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello{
 	public static void main(String args[]){
 		System.out.println("hello world");
 		System.out.println("hello Zhangnan");
+		System.out.println("hello Limeir");
 	}
 	
 }
